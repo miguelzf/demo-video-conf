3f38cbb83d4908f381b525874911a0bee17cbec3
miguelzf <eligmu@gmail.com>
2016-11-17 19:34:40 +0000

    Remove scaffold generic code

--- client/static/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import NameLoader from './view/NameLoader';
-
-import './style/index.css!';
-
-// Tell react to render the component
-render(<NameLoader staticName="demo-video-chat" />, document.getElementById('content'));
